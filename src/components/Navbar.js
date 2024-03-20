1b69d5da2541392290a90350e3013dbf9838f668
Talha <mtalhamanzoor786@gmail.com>
2024-03-20 10:57:05 -0700

    Pushing to new repository

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,21 +15,7 @@ export default function Navbar(props) {
                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">Action</a></li>
-                                    <li><a className="dropdown-item" href="/">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href='/' aria-disabled="true">Disabled</a>
+                                <a className="nav-link" href="/about">About</a>
                             </li>
                         </ul>
                         <div className={`form-check form-switch text-${props.mode === 'dark' ? 'light' : 'dark'}`}>
